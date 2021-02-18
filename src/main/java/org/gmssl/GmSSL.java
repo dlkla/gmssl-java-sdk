b437e09a5813539f19cebab86b0a7319ff94ad9b
zhiliao <zhiliaoonline@163.com>
2021-02-19 00:03:49 +0800

    根据系统来加载so/dll

--- src/main/java/org/gmssl/GmSSL.java
+++ src/main/java/org/gmssl/GmSSL.java
@@ -251,6 +251,17 @@ public class GmSSL {
 	}
 
 	static {
-		System.loadLibrary("gmssljni");
+
+		String os = System.getProperty("os.name");
+
+		if (os.contains("Windows 10") || os.contains("windows 10")) {
+			// 加载dll
+			System.loadLibrary("libcrypto-1_1-x64");
+			System.loadLibrary("gmssljni");
+		} else if (os.contains("linux")){
+			// 加载so文件
+			System.loadLibrary("gmssljni");
+		}
+
 	}
 }
